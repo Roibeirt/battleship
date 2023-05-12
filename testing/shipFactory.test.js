a18a9418d58e232c49bac56cc7b6000ff490bcc7
Bobby Aston <bobby.j.aston@gmail.com>
2023-05-12 14:52:55 +0900

    updated tests for ShipFactory

--- testing/shipFactory.test.js
+++ testing/shipFactory.test.js
@@ -1,15 +1,19 @@
-import shipFactory from "../src/shipFactory";
+import ShipFactory from "../src/ShipFactory";
 
 test("creates a ship object with correct length", () => {
-  const testShip = shipFactory(3);
+  const testShip = ShipFactory(3);
   expect(testShip.shipLength).toBe(3);
 });
 
-test("accurately tracks hits", () => {
-  const testShip = shipFactory(3);
-  expect(testShip.hits).toBe(0);
+test("accurately tracks hits and determines when ship is sunk", () => {
+  const testShip = ShipFactory(3);
+  expect(testShip.isSunk()).toBe(false);
   testShip.hit();
-  expect(testShip.hits).toBe(1);
+  expect(testShip.isSunk()).toBe(false);
   testShip.hit();
-  expect(testShip.hits).toBe(2);
+  expect(testShip.isSunk()).toBe(false);
+  testShip.hit();
+  expect(testShip.isSunk()).toBe(true);
+  testShip.hit();
+  expect(testShip.isSunk()).toBe(true);
 });
